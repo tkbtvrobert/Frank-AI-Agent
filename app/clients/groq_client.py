1752c6057c0846aec6e108abe673816471a941c9
tkbtvrobert <91646085+tkbtvrobert@users.noreply.github.com>
2026-07-13 23:06:39 +0700

    Implement exception handling in GroqClient

--- app/clients/groq_client.py
+++ app/clients/groq_client.py
@@ -1,6 +1,27 @@
+"""
+GroqClient is responsible for:
+
+1. Communicating with the Groq API
+
+2. Sending messages
+
+3. Returning LLM responses
+
+4. Handling API-level exceptions
+
+Not responsible for:
+
+- Managing History
+
+- Managing Prompts
+
+- Managing Workflows
+"""
+
 from groq import Groq
 from app.config import GROQ_API_KEY
 from app.config import GROQ_MODEL
+from groq import (APITimeoutError, AuthenticationError, APIConnectionError)
 
 class GroqClient:
     def __init__(self):
@@ -9,5 +30,10 @@ class GroqClient:
         )
 
     def chat(self, messages):
-        response = self.client.chat.completions.create(messages=messages, model=GROQ_MODEL)
+        try:
+            response = self.client.chat.completions.create(messages=messages, model=GROQ_MODEL)
+        except AuthenticationError as e:
+            raise
+        except APITimeoutError as e:
+            raise
         return response.choices[0].message.content
